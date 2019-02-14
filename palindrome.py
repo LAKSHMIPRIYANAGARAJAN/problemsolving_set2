611e9b20c66a75cd2a1c5020fe63c57140068a6a
LAKSHMIPRIYANAGARAJAN <47146230+LAKSHMIPRIYANAGARAJAN@users.noreply.github.com>
2019-02-14 21:46:59 +0530

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,13 +1,10 @@
 n=int(input())
 num=n
 pal=0
-if n>1000:
-	print("invalid")
-else:
-    while num<=1000 and num>0:
-	     mod=num%10
-	     pal=(pal*10)+mod
-	     num=num//10
+while num<=1000 and num>0:
+     mod=num%10
+     pal=(pal*10)+mod
+     num=num//10
 if pal==n:
 	print("yes")
 else :
